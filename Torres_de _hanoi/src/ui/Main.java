91bb0843adba5b5aefe6d8f252f59f15dd1e0a00
Juan Felipe <jufeplasa@hotmail.com>
2021-11-06 02:55:53 -0500

    second commit

--- Torres_de _hanoi/src/ui/Main.java
+++ Torres_de _hanoi/src/ui/Main.java
@@ -16,6 +16,7 @@ public class Main {
 		scanner.nextLine();
 		disk = new int[m];
 		enterDisk(0);
+		start(0);
 	}
 
 	public static void enterDisk(int i) {
@@ -24,5 +25,39 @@ public class Main {
 			enterDisk(i+1);
 		}
 	}
-	
+
+	public static void start(int i) {
+		if(i<disk.length) {
+			tower[0]=disk[i];
+			tower[1]=0;
+			tower[2]=0;
+			showTowers();
+			process(disk[i],0,1,2);
+			System.out.println("");
+			start(i+1);
+		}
+
+	}
+
+	public static void process( int disks,int origin,int aux, int finish ) {
+
+		
+			if(disks==1) {
+				tower[origin]--;
+				tower[finish]++;
+				showTowers();
+			}
+			else {
+				process(disks-1, origin , finish ,aux);
+				tower[origin]--;
+				tower[finish]++;
+				showTowers();
+				process(disks-1, aux, origin ,finish);
+			}	
+		
+	}
+
+	public static void showTowers() {
+		System.out.println(tower[0]+" "+tower[1]+" "+tower[2]);
+	}
 }
